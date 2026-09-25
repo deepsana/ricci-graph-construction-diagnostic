e080ef07402706963cee78c41716bb9b3b1ccf0c
Deepsana Shahi <deepsanashahi@gmail.com>
2026-09-25 16:48:23 -0700

    wip
    Added QM9_DOWNLOADED_DATA variable for QM9 data path.

--- paths.py
+++ paths.py
@@ -9,6 +9,9 @@ JETSET_SHARD_ROOT = "/path/to/JetSet/shards"
 # the shard root to reuse a cache that an earlier version wrote there.
 JETSET_CACHE_DIR = os.path.join(REPO_ROOT, "cache", "jetset")
 
+# QM9 path to where it was downloaded
+QM9_DOWNLAODED_DATA = "/path/to/qm9/"
+
 # QM9 shards, as a glob pattern.
 QM9_SHARD_GLOB = "/path/to/qm9/graphs/temp_train/all_train_*.h5"
 
